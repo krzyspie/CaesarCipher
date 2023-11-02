5dc7707c2d15a10ea70b473de88c280d2b95e95a
krzysiek <krzys_7@op.pl>
2023-11-02 20:46:01 +0100

    Add algorithm initialization

---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, lets encode your message!");
+Console.WriteLine("Type your message:");
+string messageToEncode = Console.ReadLine();
+Console.WriteLine("Enter the algorithm shift:");
+string shift = Console.ReadLine();
