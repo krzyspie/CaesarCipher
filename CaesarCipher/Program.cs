fa7096d71a43caf1f5a9b2232a11f5611e03162e
krzysiek <krzys_7@op.pl>
2023-11-07 21:57:20 +0100

    Move alphabet dictionary to separate static class

--- CaesarCipher/Program.cs
+++ CaesarCipher/Program.cs
@@ -1,51 +1,22 @@
-﻿using System.Text;
-
-Dictionary<int, char> aplhabetMap = new Dictionary<int, char>()
-{
-    { 0, 'a' },
-    { 1, 'b' },
-    { 2, 'c' },
-    { 3, 'd' },
-    { 4, 'e' },
-    { 5, 'f' },
-    { 6, 'g' },
-    { 7, 'h' },
-    { 8, 'i' },
-    { 9, 'j' },
-    { 10, 'k' },
-    { 11, 'l' },
-    { 12, 'm' },
-    { 13, 'n' },
-    { 14, 'o' },
-    { 15, 'p' },
-    { 16, 'q' },
-    { 17, 'r' },
-    { 18, 's' },
-    { 19, 't' },
-    { 20, 'u' },
-    { 21, 'v' },
-    { 22, 'w' },
-    { 23, 'x' },
-    { 24, 'y' },
-    { 25, 'z' },
-};
+﻿using CaesarCipher;
+using System.Text;
 
 Console.WriteLine("Hello, lets encode your message!");
 
 string messageToEncode = GetMessageToEncode();
 
-int algorithmShift = GetAlgorithmShift(aplhabetMap);
+int algorithmShift = GetAlgorithmShift(Constants.aplhabetMap);
 
 StringBuilder encodedMessage = new StringBuilder();
 foreach (var stringCharacter in messageToEncode)
 {
     char encodedLetter = stringCharacter;
 
-    if (aplhabetMap.ContainsValue(stringCharacter))
+    if (Constants.aplhabetMap.ContainsValue(stringCharacter))
     {
-        var letterKey = aplhabetMap.FirstOrDefault(y => y.Value == stringCharacter).Key;
+        var letterKey = Constants.aplhabetMap.FirstOrDefault(y => y.Value == stringCharacter).Key;
         var encodedLetterKey = 26 + (letterKey + algorithmShift);
-        encodedLetter = aplhabetMap[encodedLetterKey % aplhabetMap.Count];
+        encodedLetter = Constants.aplhabetMap[encodedLetterKey % Constants.aplhabetMap.Count];
     }
 
     encodedMessage.Append(encodedLetter);
